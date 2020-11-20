5a054240548d780045191e5e030f36b7651770ef
hobedtech <info@hobedtech.com>
2020-11-20 12:19:33 +0300

    Initial Commit

---
+++ Json_WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Json_WeatherApp
+//
+//  Created by Berker Vergi on 20.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
